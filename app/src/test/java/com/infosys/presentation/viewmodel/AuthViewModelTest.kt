1415f23fa9fdd7ef8024802b393fc91cd66ec8fc
nisha <nisha.saini.d136@gmail.com>
2025-03-16 09:39:49 +0530

    - authviewmodel test case with mockito

--- app/src/test/java/com/infosys/presentation/viewmodel/AuthViewModelTest.kt
+++ app/src/test/java/com/infosys/presentation/viewmodel/AuthViewModelTest.kt
@@ -4,37 +4,59 @@ import app.cash.turbine.test
 import com.infosys.data.model.usecase.AuthUseCase
 import com.infosys.data.model.user.User
 import com.infosys.data.remote.Resource
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.mockk
+import com.infosys.domain.repository.ClearUserInfoLocalRepository
+import com.infosys.domain.repository.ReadUserInfoLocalRepository
+import com.infosys.domain.repository.WriteUserInfoLocalRepository
+import com.infosys.domain.usecase.ClearUserInfoLocalUseCase
+import com.infosys.domain.usecase.ReadUserInfoLocalUseCase
+import com.infosys.domain.usecase.WriteUserInfoLocalUseCase
+import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 
-@RunWith(MockitoJUnitRunner::class)
+//@RunWith(MockitoJUnitRunner::class)
 class AuthViewModelTest {
 
     private lateinit var authViewModel: AuthViewModel
-    private var useCase: AuthUseCase = mockk<AuthUseCase>(relaxed = true)
+    private lateinit var useCase: AuthUseCase
+    private lateinit var read: ReadUserInfoLocalUseCase
+    @Mock private lateinit var readRepo: ReadUserInfoLocalRepository
+    private lateinit var write: WriteUserInfoLocalUseCase
+    @Mock private lateinit var writeRepo: WriteUserInfoLocalRepository
+    private lateinit var clear: ClearUserInfoLocalUseCase
+    @Mock private lateinit var clearRepo: ClearUserInfoLocalRepository
     private val dispatcher = StandardTestDispatcher()
+    private lateinit var user: User
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Before
     fun init() {
+        MockitoAnnotations.initMocks(this)
         Dispatchers.setMain(dispatcher)
+        read = ReadUserInfoLocalUseCase(readRepo)
+        write = WriteUserInfoLocalUseCase(writeRepo)
+        clear = ClearUserInfoLocalUseCase(clearRepo)
+
+        useCase = AuthUseCase(read, write, clear)
+
         authViewModel = AuthViewModel(useCase)
+
+        user = User("Nisha", "test@yopmail.com", "pwd")
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -43,72 +65,78 @@ class AuthViewModelTest {
         Dispatchers.resetMain()
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun `write user information from database returns SUCCESS`() = runTest {
-        coEvery { useCase.writeUserInfoLocalUseCase.writeUserInfo(User()) } returns Unit
+        `when`(
+            useCase.writeUserInfoLocalUseCase.writeUserInfo(user)
+        ).thenReturn(Unit)
 
-        val result = authViewModel.writeUserInfo(User())
-        advanceUntilIdle()
+        val result = authViewModel.writeUserInfo(user)
+        dispatcher.scheduler.advanceUntilIdle()
+        assertEquals(Unit, result)
         assertTrue(result == Unit)
 
-        coVerify { useCase.writeUserInfoLocalUseCase.writeUserInfo(User()) }
+//        verify(useCase.writeUserInfoLocalUseCase).writeUserInfo(user)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun `read user information from database returns SUCCESS`() = runTest {
-        coEvery { useCase.readUserInfoLocalUseCase.getUserInfo() } returns flowOf(User())
+        `when`(
+            useCase.readUserInfoLocalUseCase.getUserInfo()
+        ).thenReturn(flowOf(user))
 
         authViewModel.readUserInfo()
-        advanceUntilIdle()
+        dispatcher.scheduler.advanceUntilIdle()
         authViewModel.userInfo.test {
             val result = awaitItem()
             assertTrue(result is Resource.Success)
         }
 
-        coVerify { useCase.readUserInfoLocalUseCase.getUserInfo() }
+//        verify(useCase.readUserInfoLocalUseCase).getUserInfo()
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun `read user information from database return null`() = runTest {
-        coEvery { useCase.readUserInfoLocalUseCase.getUserInfo() } returns flowOf(null)
+        `when`(
+            useCase.readUserInfoLocalUseCase.getUserInfo()
+        ).thenReturn(flowOf(null))
 
         authViewModel.readUserInfo()
-        runCurrent()
+        dispatcher.scheduler.advanceUntilIdle()
         authViewModel.userInfo.test {
             val result = awaitItem()
             assertTrue(result is Resource.Error)
         }
 
-        coVerify { useCase.readUserInfoLocalUseCase.getUserInfo() }
+//        verify(useCase.readUserInfoLocalUseCase).getUserInfo()
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun `read user information from database throws EXCEPTION`() = runTest {
-        coEvery { useCase.readUserInfoLocalUseCase.getUserInfo() } throws Exception("Test Exception")
+        `when`(
+            useCase.readUserInfoLocalUseCase.getUserInfo()
+        ).thenThrow(RuntimeException::class.java)
 
         authViewModel.readUserInfo()
-        advanceUntilIdle()
+        dispatcher.scheduler.advanceUntilIdle()
         authViewModel.userInfo.test {
             val result = awaitItem()
             assertTrue(result is Resource.Error)
         }
 
-        coVerify { useCase.readUserInfoLocalUseCase.getUserInfo() }
+//        verify(useCase.readUserInfoLocalUseCase).getUserInfo()
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun `clear user information from database returns SUCCESS`() = runTest {
-        coEvery { useCase.clearUserInfoLocalUseCase.clearUserInfo() } returns Unit
+        `when`(
+            useCase.clearUserInfoLocalUseCase.clearUserInfo()
+        ).thenReturn(Unit)
 
         val result = authViewModel.clearUserInfo()
-        advanceUntilIdle()
+        dispatcher.scheduler.advanceUntilIdle()
         assertTrue(result == Unit)
 
-        coVerify { useCase.clearUserInfoLocalUseCase.clearUserInfo() }
+//        verify(useCase.clearUserInfoLocalUseCase).clearUserInfo()
     }
 }
